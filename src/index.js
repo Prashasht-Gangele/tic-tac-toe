57bd94b0207f8752a235fafd1e20c6386b2d6560
Prashasht-Gangele <93002961+Prashasht-Gangele@users.noreply.github.com>
2022-01-01 12:42:59 +0530

    final1

--- src/index.js
+++ src/index.js
@@ -1,255 +1,120 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Prashasht Gangele Resume</title>
-	 <link rel="icon" type="image/x-icon" href="logo.png">
-	<link rel="stylesheet" href="style.css">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-	  <meta charset="UTF-8">
-  <meta name="Prashasht Gangele" content="Portfolio">
-  <meta name="Prashasht Gangele, Resume, Frontend, Backend, React, Fullstack Devloper, Web Devlopment" content="HTML, CSS, JavaScript">
-  <meta name="author" content="John Doe">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-	<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-</head>
-<body>
-
-<!---------------Header--------------->
-<section id="header">
-	<div class="container text-center">
-	<div class="user-box">
-	<img src="manu.jpg">	
-	<h1>Prashasht Gangele</h1>
-	<p>Freelance Front End Devloper</p>
-	</div>
-	</div>
-	<div class="scroll-btn">
-	<div class="scroll-bar"> <a href="#about"><span> </span></a></div>
-</div>
-</section>
-<!------------------- user info ------------------->
-<section id="user-info">
-<div class="nav-bar">
-	<nav class="navbar navbar-expand-lg">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="logo.png"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <i class="fas fa-bars"></i>
+const Square = (props) => {
+  return (
+    <button 
+      className='square'
+      onClick={props.onClick}
+    >
+      {props.value}
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#top">HOME</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#about">ABOUT ME</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#resume">RESUME</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#services">PROJECT</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#contact">CONTACT</a>
-        </li>
-      </ul>
-    </div>
-</nav>
- </div>
-<!-- -------------------------ABOUT--------------- -->
+  );
+}
 
-<div class="about conatiner" id="about">
-<div class="row">
-<div class="col-md-6 text-center">
-<img src="profile1.jpg" class="profile-img">
-</div>
-<div class="col-md-6">
-	<h3>Who I am</h3>
-	<p> My recently finished Newton School Full Stack Devloper Course has shaped my information architecture, data structure, and agile web development knowledge, and I know I have the HTML, CSS, and JS skills to help Steinway Solutions succeed.
-<div class="skills-bar">
-<p>Java</p>
-<div class="progress">
-<div class="progress-bar" style="width: 80%">80%</div> >
-</div>
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
 
-<p>JS Skill</p>
-<div class="progress">
-<div class="progress-bar" style="width: 80%">80%</div> >
-</div>
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    })
+  }
 
-<p>HTML</p>
-<div class="progress">
-<div class="progress-bar" style="width: 95%">95%</div> >
-</div>
+  renderSquare(i) {
+    return (
+      <Square
+        value={ this.state.squares[i] } 
+        onClick={ () => this.handleClick(i) }
+      />
+    );
+  }
 
-<p>CSS</p>
-<div class="progress">
-<div class="progress-bar" style="width: 90%">90%</div> >
-</div>
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+       status = 'Winner ' + winner;
+   } else { 
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;  
+    }
 
-<p>React</p>
-<div class="progress">
-<div class="progress-bar" style="width: 80%">80%</div> >
-</div>
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
 
-<p>MS Project</p>
-<div class="progress">
-<div class="progress-bar" style="width: 75%">75%</div> >
-</div>
-	
-<p>Auto CAD</p>
-<div class="progress">
-<div class="progress-bar" style="width: 85%">85%</div> >
-</div>
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
 
-<p>Primavera</p>
-<div class="progress">
-<div class="progress-bar" style="width: 70%">70%</div> >
-</div>
-</div>
-</div>
-</div>
-</div>
-<!-- ------------social icons ---------------->
-<div class="social-icons">
-	<ul>
-		<a href="https://www.linkedin.com/in/prashasht-gangele-099492156"><li><i class="fa fa-linkedin"></i></li></a>
-		<a href="mailto:name@rapidtables.com"><li><i class="fa fa-envelope"></i></li></a>
-		<a href="https://github.com/Prashasht-Gangele"><li><i class="fa fa-github"></i></li></a>
-		<a href="https://drive.google.com/file/d/1ge5-Gz_HI1dvVTV-YtiH0-pmw6MoN1pm/view?usp=sharing"><li><i class="fa fa-download"></i></li></a>
-		
-	</ul>
-</div>
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ]
 
-<!-- ---------------------------resume time line ----------------------->
-<div class="resume" id="resume">
-<div class="container">
-<div class="row">
-<div class="col-md-6">
-<h3 class="text-center">My Experiences</h3>
-<ul class="timeline">
+  for (let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
-	<li>
-	<h4><span>2018 - </span>Planning Engineer</h4>
-	<p>Responsible for Site Management, Material management, Work Schedule, Progress Report, Bill reconciliation, Manpower planning.</p>
-	<p>Using project planning tools like MS Package and MS Project, EPC-Promen, Primavera, Auto CAD.</p>
-	<p>Accountable for Erection, Amendment, Mechanical clearance, Pre-Commissioning, Study of ISO & P&ID drawings, BOM, MTO.</p>
-		
-	<b>company</b> -Onshore Construction Pvt Ltd<br>
-	<b>Experiences</b> -2yr [2019 to 2020]<br>
-	<b>Location</b> -Mumbai
-	</p>	
-	</li>
-</ul>	
-</div>
+// ========================================
 
-<div class="col-md-6">
-<h3 class="text-center">My Education</h3>
-<ul class="timeline">
-	<li>
-	<h4><span>2021- </span>Full Stack Devloper Course</h4>
-		<p>Full stack web development along with problem Solving</p>
-		<p>Technical Stack: JAVA, HTML, CSS, JS and React.</p>
-		<p>Participated various coding contest organized by the plateform.</p>
-		<p>Worked on various projects like Lyrics app, Responsive CV.</p>
-
-		<b>Institute</b> - Newton School<br>
-		<b>Course</b> -Mechanical<br>
-		<b>Duration</b> - july 2021 - jan 2021
-</p>	
-</li>
-<li>
-	<h4><span>2018- </span>Bachlor Of Engineering</h4>
-	<p>
-
-		<b>University</b> -Rajiv Gandhi Prodhyogiki Vishvidhaylya Bhopal MP <br>
-		<b>Branch</b> -Mechanical<br>
-		<b>Pass-Out year</b> -2018
-</p>	
-</li>
-</ul>	
-</div>
-</div>
-</div>
-</div>
-
-<!-- =================================services =====================================-->
-<div class="services" id="services">
-<div class="container">
-<h1 class="text-center">Projects</h1>
-<p class="text-center"></p>	
-<div class="row">
-<div class="col-md-4" style="cursor:pointer;" onClick="window.open('https://0l5pc.csb.app/','_newtab');">
-<div class="services-box">
-<i class="fa fa-rocket"></i><span>Tic-Tac-Toe APP</span>
-	<p><ul>
-	<li>Built a cross circle game.</li>
-	<li>Used React hooks.</li>
-	<li>Tech Stack: React JS, JavaScript, HTML, CSS.</li></ul></p>			
-</div>
-</div>
-<div class="col-md-4" style="cursor:pointer;" onClick="window.open('https://y6rlm.csb.app/','_newtab');">
-<div class="services-box">
-<i class="fa fa-table"></i><span>TV-Show-APP</span>
-	<p><ul><li>Built a website which give information about US shows charecter.</li>
-	<li>Used fetch API and make single page web.</li>
-	<li>Tech Stack: React JS, JavaScript, HTML, CSS.</li></ul></p>		
-</div>
-</div>
-<div class="col-md-4" style="cursor:pointer;" onClick="window.open('https://prashasht-gangele.github.io/lyricsapp.github.io/','_newtab');">
-<div class="services-box">
-<i class="fa fa-laptop"></i><span>Lyrics APP</span>
-	<p>
-	<ul><li>Built a search engine for lyrics.</li>
-	<li>Used asynchronous programming and fetch API to get lyrics of songs.</li>
-	<li>Used fetch API and async await to get lists of songs and their corresponding lyrics.</li>
-	<li>Tech Stack: HTML,JavaScript, CSS.</li></ul> 
- </p>			
-</div>
-</div>
-
-</div>
-</div>
-</div>
-
-<!-- -----------------------------contact------------------------ -->
-<div class="contact" id="contact">
-<div class="conatiner text-center">
-<h1>Contact Me</h1>
-<p class="text-center">If you want conatact me and you have any opportunity for me</p><br>
-<div class="row">
-<div class="col-md-4">
-<i class="fa fa-phone"></i>
-<p>+917697052815</p>
-</div>
-<div class="col-md-4">
-<i class="fa fa-envelope"></i>
-<p>prashashtgangele@gmail.com</p>
-</div>
-<div class="col-md-4">
-<i class="fa fa-linkedin"></i>
-<p>https://www.linkedin.com/in/prashahst-gangele-099492156</p>
-</div>
-</div>
-<button><a class="btn-btn-primary" href="https://drive.google.com/file/d/1ge5-Gz_HI1dvVTV-YtiH0-pmw6MoN1pm/view?usp=sharing"><i class="fa fa-download"></i>Resume</a></button>
-<button><a class="btn-btn-primary" href="mailto:name@rapidtables.com"><i class="fa fa-rocket"></i>Hire Me</a></button>
-</div>
-<div class="footer text-center"><p>made by<i class="fa fa-heart-o"></i>  Prashahst Gangele</p>
-</div>
-</div>
-</section>
-
-<!-- -------------------smooth scroll---------------------- -->
-<!-- <script type="smooth-scroll.js"></script> -->
-
-<script>
-	var scroll = new SmoothScroll('a[href*="#"]');
-	document.getElementById(tv).
-</script>
-
-</body>
-</html>
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
